890f305ffffc16e61b061ce8a56c0a821ccc6747
slimBN <elliji@yopmail.com>
2014-02-05 14:27:18 +0100

    root user

--- config/deploy.rb
+++ config/deploy.rb
@@ -22,4 +22,4 @@ end
 set :stage, :production
 
 # Replace 127.0.0.1 with your server's IP address!
-server '37.187.70.117', user: 'deploy', roles: %w{web app}
+server '37.187.70.117', user: 'root', roles: %w{web app}
